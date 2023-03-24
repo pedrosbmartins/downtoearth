a5875441e98f5289615f2155d3670fbc73b0a775
Pedro Martins <pedrosbarbosamartins@gmail.com>
2023-03-23 21:16:59 -0300

    Refactor main logic to App class

---
+++ src/App.ts
@@ -0,0 +1,88 @@
+import { SizePresets } from './components/dom'
+import { SidebarItem } from './components/dom/SidebarItem'
+import * as mapComponents from './components/map'
+import map from './map'
+import { GroupStore, ModelData, ModelStore, RootStore, UnitStore } from './store'
+import { matchEvent } from './store/core'
+import { Config, Group, Model, Root } from './types'
+import { $sidebar } from './ui'
+
+export default class App {
+  private unitStore: UnitStore
+  private rootStore: RootStore | undefined
+
+  constructor() {
+    this.unitStore = new UnitStore(undefined)
+  }
+
+  public initialize(config: Config) {
+    const { root, groups } = config
+    let rootMapComponent: mapComponents.Root | undefined
+    if (root) {
+      const { store, mapComponent } = this.buildRoot(root)
+      this.rootStore = store
+      rootMapComponent = mapComponent
+      map.on('click', event => {
+        this.rootStore?.set({ center: event.lngLat.toArray() })
+      })
+    }
+    this.unitStore = new UnitStore(this.rootStore)
+    const builtGroups = groups?.map(group => this.buildGroup(group))
+    return () => {
+      $sidebar.innerHTML = ''
+      this.rootStore?.destroy()
+      this.rootStore = undefined
+      rootMapComponent?.destroy()
+      builtGroups?.forEach(group => {
+        group.store.destroy()
+        group.builtModels.forEach(model => {
+          model.store.destroy()
+          model.mapComponent.destroy()
+        })
+      })
+    }
+  }
+
+  private buildRoot(root: Root) {
+    const { label, sizePresets, layer } = root
+    const store = new RootStore(root)
+    const sizePresetsComponent = SizePresets({ presets: sizePresets }, store)
+    const itemComponent = SidebarItem({ label, children: [sizePresetsComponent] }, store)
+    $sidebar.append(itemComponent.dom())
+    let mapComponent: mapComponents.Root | undefined
+    if (layer) {
+      mapComponent = new mapComponents.Root('root', store, {
+        size: sizePresets.find(sp => sp.default)!.value,
+        layerDefinitions: [layer]
+      })
+    }
+    return { store, mapComponent }
+  }
+
+  private buildGroup(group: Group) {
+    const store = new GroupStore(`group-${group.id}`, group, this.rootStore)
+    const item = SidebarItem({ label: group.label }, store)
+    $sidebar.append(item.dom())
+    const builtModels = group.models.map(model => this.buildModel(model, store))
+    const boundingBoxModel = builtModels.find(m => m.layers.some(l => l.actAsGroupBounds))
+    if (boundingBoxModel) {
+      store.set({ boundingBox: boundingBoxModel.mapComponent.boundingBox() })
+      boundingBoxModel.store.register(store, 'boundingBox', event => {
+        if (matchEvent<ModelData>(boundingBoxModel.store.id, 'model', event)) {
+          store.set({ boundingBox: event.data.boundingBox })
+        }
+      })
+    }
+    return { store, builtModels }
+  }
+
+  private buildModel(model: Model, groupStore?: GroupStore) {
+    const store = new ModelStore(model, this.unitStore, groupStore)
+    const item = SidebarItem({ label: model.label }, store)
+    $sidebar.append(item.dom())
+    const mapComponent = new mapComponents.Regular(model.id, store, {
+      layerDefinitions: model.layers
+    })
+    return { store, mapComponent, layers: model.layers }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,30 @@
 import demo3JSON from '../setup/demo3.json'
 import solarSystemJSON from '../setup/solar-system.json'
-import { SizePresets } from './components/dom'
-import { SidebarItem } from './components/dom/SidebarItem'
-import * as mapComponents from './components/map'
+import App from './App'
 import map from './map'
-import { GroupStore, ModelData, ModelStore, RootStore, UnitStore } from './store'
-import { matchEvent } from './store/core'
-import { Config, Group, Model, Root } from './types'
-import { $configDropdown, $configFileSelector, $sidebar } from './ui'
+import { Config } from './types'
+import { $configDropdown, $configFileSelector } from './ui'
 
 const configs = {
   solarSystem: solarSystemJSON as Config,
   demo3: demo3JSON as Config
 }
 
+const app = new App()
 let destroy: (() => void) | undefined
-let rootStore: RootStore | undefined
-let unitStore: UnitStore
 
 function initialize(config: Config) {
   if (destroy) destroy()
-  const { root, groups } = config
-  let rootMapComponent: mapComponents.Root | undefined
-  if (root) {
-    const { store, mapComponent } = buildRoot(root)
-    rootStore = store
-    rootMapComponent = mapComponent
-  }
-  unitStore = new UnitStore(rootStore)
-  const builtGroups = groups?.map(group => buildGroup(group))
-  return () => {
-    $sidebar.innerHTML = ''
-    rootStore?.destroy()
-    rootStore = undefined
-    rootMapComponent?.destroy()
-    builtGroups?.forEach(group => {
-      group.store.destroy()
-      group.builtModels.forEach(model => {
-        model.store.destroy()
-        model.mapComponent.destroy()
-      })
-    })
-  }
-}
-
-function buildRoot(root: Root) {
-  const { label, sizePresets, layer } = root
-  const store = new RootStore(root)
-  const sizePresetsComponent = SizePresets({ presets: sizePresets }, store)
-  const itemComponent = SidebarItem({ label, children: [sizePresetsComponent] }, store)
-  $sidebar.append(itemComponent.dom())
-  let mapComponent: mapComponents.Root | undefined
-  if (layer) {
-    mapComponent = new mapComponents.Root('root', store, {
-      size: sizePresets.find(sp => sp.default)!.value,
-      layerDefinitions: [layer]
-    })
-  }
-  return { store, mapComponent }
-}
-
-function buildGroup(group: Group) {
-  const store = new GroupStore(`group-${group.id}`, group, rootStore)
-  const item = SidebarItem({ label: group.label }, store)
-  $sidebar.append(item.dom())
-  const builtModels = group.models.map(model => buildModel(model, store))
-  const boundingBoxModel = builtModels.find(m => m.layers.some(l => l.actAsGroupBounds))
-  if (boundingBoxModel) {
-    store.set({ boundingBox: boundingBoxModel.mapComponent.boundingBox() })
-    boundingBoxModel.store.register(store, 'boundingBox', event => {
-      if (matchEvent<ModelData>(boundingBoxModel.store.id, 'model', event)) {
-        store.set({ boundingBox: event.data.boundingBox })
-      }
-    })
-  }
-  return { store, builtModels }
-}
-
-function buildModel(model: Model, groupStore?: GroupStore) {
-  const store = new ModelStore(model, unitStore, groupStore)
-  const item = SidebarItem({ label: model.label }, store)
-  $sidebar.append(item.dom())
-  const mapComponent = new mapComponents.Regular(model.id, store, {
-    layerDefinitions: model.layers
-  })
-  return { store, mapComponent, layers: model.layers }
+  destroy = app.initialize(config)
 }
 
 map.on('load', () => {
-  destroy = initialize(configs.solarSystem)
+  initialize(configs.solarSystem)
 
   $configDropdown.addEventListener('change', function (this: HTMLSelectElement) {
     const config = configs[this.value as keyof typeof configs]
     if (config) {
-      destroy = initialize(config)
+      initialize(config)
     }
   })
 
@@ -110,10 +41,6 @@ map.on('load', () => {
     }
     const configText = await file.text()
     const config = JSON.parse(configText) as Config
-    destroy = initialize(config)
+    initialize(config)
   })
-})
-
-map.on('click', event => {
-  rootStore?.set({ center: event.lngLat.toArray() })
 })
