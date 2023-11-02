d48d267d198f183ab0d3a8729d0f37083bca9b4f
Pedro Martins <pedrosbarbosamartins@gmail.com>
2023-11-02 19:53:17 -0300

    Implement reverse geocoding using Nominatim

--- src/geocoding.ts
+++ src/geocoding.ts
@@ -1,35 +1,15 @@
-import { MAPBOXGL_ACCESS_TOKEN } from './constants'
 import { LngLat } from './types'
 
 export async function reverseGeocoding(lngLat: LngLat) {
   try {
     const [lng, lat] = lngLat
-    const response = await fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOXGL_ACCESS_TOKEN}`
-    )
+    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}&zoom=10`
+    const response = await fetch(url)
     const json = await response.json()
-
-    const query = json.query.toString() as string
-    if (json.features.length === 0) {
-      return query
+    if (json.error) {
+      return lngLat.map(v => v.toFixed(4)).toString()
     }
-
-    const mainFeature = json.features[0]
-    const context = mainFeature.context as Array<{ id: string; text: string }>
-    const locality = context.find(c => c.id.startsWith('locality'))?.text
-    const place = context.find(c => c.id.startsWith('place'))?.text
-    const region = context.find(c => c.id.startsWith('region'))?.text
-    const country = context.find(c => c.id.startsWith('country'))?.text
-
-    if (region && country) {
-      return `${region}, ${country}`
-    }
-    if (locality && place) {
-      return `${locality}, ${place}`
-    }
-    if (mainFeature.place_name) {
-      return mainFeature.place_name as string
-    }
+    return json.display_name
   } catch (e) {
     console.error(e)
     return undefined
